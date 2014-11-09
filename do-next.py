7c4b0cba4ba46b10a7c93a0ae9361a26011c3b13
Julian Schrittwieser <j.schrittwieser@gmail.com>
2014-11-09 15:13:58 +0000

    sample

---
+++ do-next.py
@@ -0,0 +1,34 @@
+import multiprocessing.dummy
+import os
+import random
+
+from tasks import wanikani, duolingo, plaintasks
+
+home = os.environ['HOME']
+todo_dir = os.path.join(home, 'Dropbox/wichtig/todo/')
+
+mandatory = [
+  wanikani.WaniKani('1f00efbe4183e455b29e5ad99f45b057'),
+  duolingo.Duolingo('JulianSchr'),
+]
+optional = [
+  plaintasks.PlainTasks(todo_dir, '@remind')
+]
+
+p = multiprocessing.dummy.Pool(max(len(mandatory), len(optional)))
+
+
+def next():
+  tasks = [t for ts in p.map(lambda s: s.tasks(), mandatory) for t in ts]
+  if tasks:
+    return tasks[0]
+
+  tasks = [t for ts in p.map(lambda s: s.tasks(), optional) for t in ts]
+  if not tasks:
+    return 'You are all done!'
+
+  return tasks[random.randint(0, len(tasks))]
+
+
+if __name__ == "__main__":
+  print next()
